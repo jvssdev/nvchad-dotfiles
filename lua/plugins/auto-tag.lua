8092462fcfffaf158bb29752fe0bff3945a0d953
jvssdev <jvssdev@hotmail.com>
2025-07-04 09:22:33 -0300

    ts-autotag update

--- lua/plugins/auto-tag.lua
+++ lua/plugins/auto-tag.lua
@@ -1,33 +1,24 @@
 return {
   "windwp/nvim-ts-autotag",
-  event = "InsertEnter",
-  opts = {
-    -- Defaults
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = true, -- Auto close on trailing </
-  },
-  -- Also override individual filetype configs, these take priority.
-  -- Empty by default, useful if one of the "opts" global settings
-  -- doesn't work well in a specific filetype
-  -- per_filetype = {
-  --   ["html"] = {
-  --     enable_close = true,
-  --   },
-  --   ["javascript"] = {
-  --     enable_close = true,
-  --   },
-  --   ["typescript"] = {
-  --     enable_close = true,
-  --   },
-  --   ["jsx"] = {
-  --     enable_close = true,
-  --   },
-  --   ["tsx"] = {
-  --     enable_close = true,
-  --   },
-  --   ["markdown"] = {
-  --     enable_close = true,
-  --   },
-  -- },
+  event = { "BufReadPre", "BufNewFile" }, -- Recommended events for lazy loading
+  dependencies = { "nvim-treesitter/nvim-treesitter" }, -- Ensure treesitter is loaded
+  config = function()
+    require("nvim-ts-autotag").setup {
+      opts = {
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = true, -- Auto close on trailing </
+      },
+      -- Override individual filetype configs, if needed
+      per_filetype = {
+        -- Example: Customize for specific filetypes
+        -- ["html"] = { enable_close = true },
+        -- ["javascript"] = { enable_close = true },
+        -- ["typescript"] = { enable_close = true },
+        -- ["jsx"] = { enable_close = true },
+        -- ["tsx"] = { enable_close = true },
+        -- ["markdown"] = { enable_close = true },
+      },
+    }
+  end,
 }
