2a296bebdd920c9d7e928b64dfbbb57d73cf7340
jvssdev <jvssdev@hotmail.com>
2025-07-18 10:56:41 -0300

    highlight text on yank

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,9 @@
 require "nvchad.autocmds"
+
+-- highlight text on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank { timeout = 200 }
+  end,
+})
