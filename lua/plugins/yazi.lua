3ac9b748fc1c789091869d394ae8a0ea38de162a
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-10-11 17:05:13 -0300

    yazi change

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -8,18 +8,18 @@ return {
   },
   keys = {
     -- 👇 in this section, choose your own keymappings!
-    -- {
-    --   "<leader>e",
-    --   mode = { "n", "v" },
-    --   "<cmd>Yazi<cr>",
-    --   desc = "Open yazi at the current file",
-    -- },
     {
-      -- Open in the current working directory
       "<leader>e",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
     },
+    -- {
+    --   -- Open in the current working directory
+    --   "<leader>e",
+    --   "<cmd>Yazi cwd<cr>",
+    --   desc = "Open the file manager in nvim's working directory",
+    -- },
     -- {
     --   "<c-up>",
     --   "<cmd>Yazi toggle<cr>",
